fe08794d57a01956b46e94c2829b2dc9e2ddd4bf
alvar soler <alvardsoler@gmail.com>
2022-11-18 10:48:14 +0000

    añadido evento para recuperar temperatura desde la BD. loggeo por stdoutput

--- burrow_bot.py
+++ burrow_bot.py
@@ -1,32 +1,17 @@
 #!/usr/bin/python3
-import logging
 import os
 import sys
 import time
+import mariadb
 from config import *
 import telepot
 from telepot.loop import MessageLoop
 
-debugging = len(sys.argv) > 1 and sys.argv[1] and sys.argv[1] == '--debug'
-
-# Config logger
-def configLogger():
-    logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-    rootLogger = logging.getLogger()
-    rootLogger.setLevel(logging.DEBUG if debugging else logging.WARN)
-
-    fileHandler = logging.FileHandler("burrow_bot.log")
-    fileHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(fileHandler)
-
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(consoleHandler)
 
 # Función principal de recepciñon de mensajes, filtra para que solo me haga caso a mi
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    logging.debug(f'Recibido mensaje. content_type: ({content_type}), chat_type: ({chat_type}), chat_id: ({chat_id})')
+    print(f'Recibido mensaje. content_type: ({content_type}), chat_type: ({chat_type}), chat_id: ({chat_id})')
 
     try:
         if chat_id == USERID:
@@ -34,14 +19,14 @@ def handle(msg):
         else:
             bot.sendMessage(USERID, f'El usuario {chat_id} está intentando mandarme mensajes!')
     except Exception as e:
-        logging.error(f'Error procesando petición {e}')
+        print(f'Error procesando petición {e}')
 
 # Depende del content_type haremos unas cosas u otras
 def handleMessage(msg, content_type):
     if content_type == 'text':
         handleTextMessage(msg['text'])
     else:
-        logging.warn(f'Recibido un content_type desconocido: {content_type}')
+        print(f'Recibido un content_type desconocido: {content_type}')
         bot.sendMessage(USERID, f'Me estás mandando algo que no es un mensaje => {content_type}')
 
 # Mensajes de tipo texto
@@ -49,8 +34,10 @@ def handleTextMessage(text):
     if text == '/conexiones':
         # mostramos las últimas conexiones del mes
         conexiones()
+    elif text == '/info-rpi':
+        infoRPI()
     elif text == '/temperatura':
-        temperatura()
+    	temperatura()
 
 # Devuelve las conexiones SSH creadas ahora
 def conexiones():
@@ -58,17 +45,39 @@ def conexiones():
     who = f.read() or 'No hay conexiones abiertas'
     bot.sendMessage(USERID, who)
 
-# Ejecuta el script temperatura que devuelve temperatura y estado de la memoria
-def temperatura():
-    f = os.popen('temperatura')
+# Ejecuta el script info-rpi.sh que devuelve temperatura y estado de la memoria
+def infoRPI():
+    f = os.popen('info-rpi.sh')
     resp = f.read() or 'Error leyendo datos'
     bot.sendMessage(USERID, resp)
 
-configLogger() # inicializamos el logger
-logging.debug('Iniciando burrow_bot...')
+def temperatura():
+    try:
+        cursor.execute("SELECT * FROM observations ORDER BY date DESC LIMIT 1;");
+        for date, temp, humidity in cursor:
+            bot.sendMessage(USERID, f"Temperatura: {temp}ºC\nHumedad: {humidity}%\nFecha de la lectura: {date}\n")
+        return
+    except mariadb.Error as e:
+        print(f"Error {e}")	
+
+
+
+try:
+    conn = mariadb.connect(
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_SCHEMA)
+    cursor = conn.cursor()
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+print('Iniciando burrow_bot...')
 bot = telepot.Bot(TOKEN)
 MessageLoop(bot, handle).run_as_thread()
-logging.debug('burrow_bot iniciado, escuchando...')
+print('burrow_bot iniciado, escuchando...')
 
 # Keep the program running.
 while 1:
